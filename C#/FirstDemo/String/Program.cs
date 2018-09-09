56db257a0bfaac4d46bf6a3806e37fa64cfed13c
Yueran-Yu <librayuyueran@gmail.com>
2018-09-09 12:21:54 -0500

    review and pracitce the HTML priority

--- C#/FirstDemo/String/Program.cs
+++ C#/FirstDemo/String/Program.cs
@@ -13,7 +13,7 @@ namespace String
             //string myString = @"Go to your c:\ drive";
             //string myString = string.Format("{1} = {0}","First", "Second");
             //string myString = string.Format("{0:C}", 123.45);
-            //string myString = string.Format("{0:N}", 1234566788);
+            string myString = string.Format("{0:C}", 12.34566788);
             //string myString = string.Format("Percentage:{0:P} ", .123);
             //string myString = string.Format("Phone Number: {0:(###) ###-####}", 123456785);
             //string myString = "Today is a nice day.  ";
@@ -29,15 +29,15 @@ namespace String
             //    myString += "--" + i.ToString();
             //}
 
-            StringBuilder myString = new StringBuilder();
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    myString.Append("--");
-                    myString.Append(i);
-                }
+            // StringBuilder myString = new StringBuilder();
+            // {
+            //     for (int i = 0; i < 100; i++)
+            //     {
+            //         myString.Append("--");
+            //         myString.Append(i);
+            //     }
 
-            }
+            // }
             Console.WriteLine(myString);
             Console.ReadLine();
         }
